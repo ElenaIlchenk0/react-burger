e8064a336af61a6b10f10ec6bd65d05275d2e406
Елена Ильченко <lenale@MacBook-Air-Elena.local>
2022-10-31 14:37:37 +0300

    fix folders name

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/_App/App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import '@ya.praktikum/react-developer-burger-ui-components';
 
